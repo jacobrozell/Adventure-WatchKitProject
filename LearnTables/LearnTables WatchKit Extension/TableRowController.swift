449c0f7be74d6f6936518728469a31ddd5119f86
Jacob Rozell <jrozell@myocv.com>
2019-12-26 12:33:05 -0600

    complete learnTables app

---
+++ LearnTables/LearnTables WatchKit Extension/TableRowController.swift
@@ -0,0 +1,16 @@
+//
+//  TableRowControlelr.swift
+//  LearnTables WatchKit Extension
+//
+//  Created by Jacob Rozell on 12/26/19.
+//  Copyright © 2019 jacobrozell. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class TableRowController: NSObject {
+    @IBOutlet var rowLabel: WKInterfaceLabel!
+    @IBOutlet var rowNum: WKInterfaceLabel!
+    
+}
